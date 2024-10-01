5b600e450cd1979af7aaf39129cadde908b5b210
Cyrax07 <talibbrother1@gmail.com>
2024-10-01 16:02:29 +0530

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using System.Data;
 using API;
+using API.Data;
 using API.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,4 +21,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
+using var  scope = app.Services.CreateScope();
+var services =  scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUser(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 app.Run();

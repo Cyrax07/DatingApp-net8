6710c194b9ac4d1e0ac05ffd4913b4731a7eafee
Cyrax07 <talibbrother1@gmail.com>
2024-10-21 23:07:06 +0530

    End of section 15

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.DTOs;
+using API.Helpers;
+
+namespace API.Interfaces;
+
+public interface IMessageRepository
+{
+    void AddMessage(Message message);
+    void DeleteMessage(Message message);
+    Task<Message?> GetMessage(int id);
+    Task<PagedList<MessageDto>> GetMessageForUser(MessageParams messageParams);
+    Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string RecipientUsername);
+    Task<bool> SaveAllAsync();
+}
